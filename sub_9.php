70b3d23f54ac553c53dbca516a2fadbe7584718c
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:09:50 +0900

    band

---
+++ sub_9.php
@@ -0,0 +1,19 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_4_01_avatarband.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_8.php">전시</a> > <strong>아바타 밴드</strong></p>
+            </div>
+            <div class="content">
+            <img src="http://robolink.co.kr/imgs/sub_img_4_avaterband_01.png">
+            <img src="http://robolink.co.kr/imgs/sub_img_4_avaterband_02.png" style="margin-top:20px;">
+            <iframe width="750" height="422" style="margin-top:30px;" src="https://www.youtube.com/embed/DchsjKUnmzQ?rel=0" frameborder="0" allowfullscreen></iframe>
+            <img src="http://robolink.co.kr/imgs/sub_img_4_avaterband_03.png">
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
