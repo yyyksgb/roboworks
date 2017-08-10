8da0a30d5f17ece522261a04b7e5316dcdac60ba
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:07:22 +0900

    info

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="title"></div>
+        <div class="main_text"></div>
+        <div class="main_info"></div>
+        <div class="category_bg">
+            <center>
+                <div style="width: 1000px; height: 450px;">
+                    <div class="category_top">
+                        <a href="sub_4.php"><img class="category_l" src="img/category_01_1.png"></a>
+                        <a href="sub_9.php"><img class="category_r" src="img/category_02.png"></a>
+                    </div>
+                    <div>
+                        <a href="sub_7.php"><img class="category_l" src="img/category_03.png"></a>
+                        <a href="sub_6.php"><img class="category_m" src="img/category_04.png"></a>
+                        <a href="sub_8.php"><img class="category_r" src="img/category_05.png"></a>
+                    </div>
+                </div>
+            </center>
+        </div>
+        <!--
+        <div class="banner">
+            <div>
+                <a href="http://roboshop.co.kr/" target="_blank"> <img class="banner_img" src="http://robolink.co.kr/imgs/main_banner_01_1.png"> </a>
+            </div>
+        </div> -->
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
---
+++ sub_1.php
@@ -0,0 +1,14 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_1_01_info.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_1.php">회사소개</a> > <strong>로보웍스란?</strong></p>
+            </div>
+            <div class="content"> <img src="img/sub_img_1_info01.png"> </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
