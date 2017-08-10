c93185823a55dbf48bf0ba39f006b7508d97f0a1
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:08:58 +0900

    cubeworksmini

---
+++ sub_6.php
@@ -0,0 +1,14 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_3_01_cubemini.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_5.php">교육 로봇</a> > <strong>큐브웍스 미니</strong></p>
+            </div>
+            <div class="content"><img src="img/20170804_mini_01.png"><img src="img/20170804_mini_02.png"><img src="img/20170804_mini_03.png"> </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
