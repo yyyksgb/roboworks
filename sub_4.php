79fb8e8668097e2c43029caa5e10019a3409e7a7
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:08:21 +0900

    cubeworksdrone

---
+++ sub_4.php
@@ -0,0 +1,14 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_2_01_cubedrone.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_4.php">드론</a> > <strong>큐브웍스 드론</strong></p>
+            </div>
+            <div class="content"> <img src="img/sub_img_2_cubeworksdrone01.png"> </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
