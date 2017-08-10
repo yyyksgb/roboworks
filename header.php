e67aeef14bd78df9e0f08fad272f4432c51032ed
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:05:18 +0900

    can edit list GNB and LNB

---
+++ header.php
@@ -0,0 +1,57 @@
+<div id="wrap">
+    <div class="top" align="center">
+        <div class="top_box">
+            <div class="logo">
+                <a href="http://www.roboworks.co.kr"><img src="img/logo.png"></a>
+            </div>
+            <div id="gnb">
+                <ul class="gnb">
+                    <li>
+                        <a href="http://www.roboworks.co.kr/sub_1.php"><img src="img/gnb_01.png" onmouseover="this.src='img/gnb_01_1.png'" onmouseout="this.src='img/gnb_01.png'" alt="회사소개"></a>
+                    </li>
+                    <li>
+                        <a href="http://www.roboworks.co.kr/sub_4.php"><img src="img/gnb_02.png" onmouseover="this.src='img/gnb_02_1.png'" onmouseout="this.src='img/gnb_02.png'" alt="드론"></a>
+                    </li>
+                    <li>
+                        <a href="http://www.roboworks.co.kr/sub_6.php"><img src="img/gnb_03.png" onmouseover="this.src='img/gnb_03_1.png'" onmouseout="this.src='img/gnb_03.png'" alt="교육 로봇"></a>
+                    </li>
+                    <li>
+                        <a href="http://www.roboworks.co.kr/sub_9.php"><img src="img/gnb_04.png" onmouseover="this.src='img/gnb_04_1.png'" onmouseout="this.src='img/gnb_04.png'" alt="전시"></a>
+                    </li>
+                    <li class="line_end">
+                        <a href="http://roboshop.co.kr/" target="_blank"><img src="img/gnb_05.png" onmouseover="this.src='img/gnb_05_1.png'" onmouseout="this.src='img/gnb_05.png'" alt="쇼핑몰"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="gnb_layer" id="effect">
+        <center>
+            <div class="grid">
+                <div class="gnb_layerInner">
+                    <ul class="g_menu_1">
+                        <li><a href="http://www.roboworks.co.kr/sub_1.php">로보웍스란?</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_2.php">회사연혁</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_3.php">뉴스</a></li>
+                    </ul>
+                    <ul class="g_menu_2">
+                        <li><a href="http://www.roboworks.co.kr/sub_4.php">큐브웍스 드론</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_5.php">로킷 드론</a></li>
+                    </ul>
+                    <ul class="g_menu_3">
+                        <li><a href="http://www.roboworks.co.kr/sub_6.php">큐브웍스 미니</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_7.php">큐브웍스 오리지날</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_8.php">언플러그드 코딩</a></li>
+                    </ul>
+                    <ul class="g_menu_4">
+                        <li><a href="http://www.roboworks.co.kr/sub_9.php">아바타 밴드</a></li>
+                        <li><a href="http://www.roboworks.co.kr/sub_10.php">아바타 모션</a></li>
+                    </ul>
+                    <ul class="g_menu_5">
+                        <li><a href="http://roboshop.co.kr/" target="_blank">로보링크샵</a></li>
+                    </ul>
+                </div>
+            </div>
+        </center>
+    </div>
+</div>
