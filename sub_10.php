2cf8ce4305480775463f3c841a122a7059971113
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:10:21 +0900

    avartamotion

---
+++ sub_10.php
@@ -0,0 +1,16 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_4_02_avatarmotion.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_8.php">전시</a> > <strong>아바타 모션</strong></p>
+            </div>
+            <div class="content">
+            <img src="http://robolink.co.kr/imgs/sub_img_4_avatermotion.png">
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
