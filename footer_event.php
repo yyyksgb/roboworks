dbd4a5fb5a3c15abab69faad6eb66bb53115787c
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 14:33:42 +0900

    footer(only use event)

---
+++ footer_event.php
@@ -0,0 +1,13 @@
+<div class="footer_ent" align="center"
+  style="max-width: 100%;margin: 0; background-color: #1d1c1d; padding: 0; width: 100%; height: 101px;">
+    <div class="footer_box">
+        <div class="footer_logo">
+            <a href="http://www.roboworks.co.kr"><img src="img/footer_logo_b.png"></a>
+        </div>
+        <div class="footer_textbox">
+            <p style="color:#fff;">대표 : 이현종&nbsp;&nbsp;|&nbsp;&nbsp;주소 : 인천광역시 남구 경인로 229번지 305호 (도화동, 인천IT타워)
+                <br> Mail : info@roboworks.co.kr&nbsp;&nbsp;|&nbsp;&nbsp;Tel : 070-7596-3531&nbsp;&nbsp;|&nbsp;&nbsp;Fax : 02-554-6399
+                <br> © Roboworks inc.</p>
+        </div>
+    </div>
+</div>
