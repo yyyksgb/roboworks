829674bf6826aec3a681547274b452ce2e347332
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 14:40:48 +0900

    cubeworks step0 open event

---
+++ 20170810_news_cubework_open.php
@@ -0,0 +1,56 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <style>
+        .title{
+            height: 689px;
+            background: url(img/20170418_titleimg.png) 50% 0 no-repeat;
+        }
+        .info{
+            height: 499px;
+            background: url(img/20170418_info.png) 50% 0 no-repeat;
+        }
+        .info2{
+            height: 397px;
+            background: url(img/20170418_nice.png) 50% 0 no-repeat;
+            margin-bottom: 60px;
+        }
+        .list{
+            height: 1090px;
+            background: url(img/20170418_list.png) 50% 0 no-repeat;
+            margin-bottom: 83px;
+        }
+        .sale{
+            height: 900px;
+            background: url(img/20170418_sale.png) 50% 0 no-repeat;
+            margin-bottom: 20px;
+        }
+        .bt{
+            text-align: center;
+        }
+        .foot{
+            height: 416px;
+            background: url(img/20170418_footer.png) 50% 0 no-repeat;
+            margin-top: 100px;
+        }
+    </style>
+    <center>
+    <div class="title"></div>
+    <div class="info"></div>
+    <div class="info2"></div>
+    <div class="list"></div>
+    <div class="sale"></div>
+    <div style="bt">
+        <a href="http://auction.kr/iBUbqJB" target="_blank">
+            <img src="img/20170418_bt_market.png">
+        </a>
+        &nbsp;&nbsp;
+        <a href="http://robolink.cafe24.com/product/detail.html?product_no=250&cate_no=29&display_group=1" target="_blank">
+            <img src="img/20170418_bt_shop.png">
+        </a>
+    </div>
+    <div class="foot"></div>
+   </center>
+    <?
+include_once dirname(__FILE__)."/footer_event.php";
+?>
