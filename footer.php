265e68d94affab3059bd8c2dabd6dce222809cde
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:06:25 +0900

    footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="footer" align="center">
+    <div class="footer_box">
+        <div class="footer_logo">
+            <a href="http://www.roboworks.co.kr"><img src="img/footer_logo.png"></a>
+        </div>
+        <div class="footer_textbox">
+            <p>대표 : 이현종&nbsp;&nbsp;|&nbsp;&nbsp;주소 : 인천광역시 남구 경인로 229번지 305호 (도화동, 인천IT타워)
+                <br> Mail : info@roboworks.co.kr&nbsp;&nbsp;|&nbsp;&nbsp;Tel : 070-7596-3531&nbsp;&nbsp;|&nbsp;&nbsp;Fax : 02-554-6399
+                <br> © Roboworks inc.</p>
+        </div>
+    </div>
+</div>
