14cb30b1f0370f04f6692380e9a67e458245dd37
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:09:15 +0900

    cubeworksorg

---
+++ sub_7.php
@@ -0,0 +1,31 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+   <style>
+       
+       .orititle {
+            height: 1260px;
+            background: url(img/20170809_cubeworks_original_01.png) 50% 0 no-repeat;
+        }
+
+
+    </style>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_3_02_cubeoriginal.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_5.php">교육 로봇</a> > <strong>큐브웍스 오리지날</strong></p>
+            </div>
+            <div class="content">
+            <div class="orititle">
+            <iframe style="margin-top:227px;" width="738px" height="410px" src="https://www.youtube.com/embed/1s00tWAhZxg?rel=0" frameborder="0" allowfullscreen></iframe></div>
+            <img src="img/20170809_cubeworks_original_02_1.png">
+            <img src="img/20170809_cubeworks_original_03.png">
+            <img src="img/20170809_cubeworks_original_04.png" style="margin-bottom:80px;">
+            <img src="img/20170809_cubeworks_original_05.png" style="margin-bottom:80px;">
+            <img src="img/20170810_cubeworks_original_01.png" style="margin-bottom:20px;">
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
