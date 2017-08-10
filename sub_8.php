7cdc0311d9f7525ce6889856610ebf219890b987
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:09:38 +0900

    unpluged

---
+++ sub_8.php
@@ -0,0 +1,29 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_3_03_cubeunplug.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_5.php">교육 로봇</a> > <strong>큐브웍스 언플러그드 코딩</strong></p>
+            </div>
+            <div class="content">
+            <img src="img/sub_img_3_unplug01.png">
+            <img src="img/sub_img_3_unplug02.png">
+            <img src="img/sub_img_3_unplug03.png">
+            <img src="img/sub_img_3_unplug04.png">
+            <img src="img/sub_img_3_unplug05.png">
+            <img src="img/sub_img_3_unplug06.png">
+            <img src="img/sub_img_3_unplug07.png">
+            <img src="img/sub_img_3_unplug08.png">
+            <img src="img/sub_img_3_unplug09.png">
+            <iframe width="750" height="422" style="margin-top:30px;" src="https://www.youtube.com/embed/Cm8F3jhOhOc?rel=0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="750" height="422" style="margin-top:10px;" src="https://www.youtube.com/embed/ROlYXXnEY7Y?rel=0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="750" height="422" style="margin-top:10px;" src="https://www.youtube.com/embed/FsIrlRdk0Js?rel=0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="750" height="422" style="margin-top:10px;" src="https://www.youtube.com/embed/EcmmZziqdCc?rel=0" frameborder="0" allowfullscreen></iframe>
+            <iframe width="750" height="422" style="margin-top:10px;" src="https://www.youtube.com/embed/TGFwY2flzD8?rel=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
