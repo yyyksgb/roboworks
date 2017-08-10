94edb5f7bb6a13b5773b8814d25d21a53d91ce76
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:08:37 +0900

    rokitdrone

---
+++ sub_5.php
@@ -0,0 +1,23 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_2_02_rokitdrone.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_4.php">드론</a> > <strong>로킷 드론</strong></p>
+            </div>
+            <div class="content">
+                <iframe width="750px" height="422px" src="https://www.youtube.com/embed/-ImkKP1rIf8?rel=0" frameborder="0" allowfullscreen></iframe>
+                <img style="margin:60px 0 30px 0;" src="img/20170802_droneimg_rokit_1_1.png">
+                   <div style="margin:0 0 50px; 0;">
+                        <a href="http://rokitarduino.org/drone-simulator/" target="_blank"> <img style="margin-right:20px;" src="img/20170802_droneimg_rokit_2.png"></a>
+                        <a href="http://rokitarduino.org/2016/03/04/%EB%A1%9C%ED%82%B7-%EB%B8%8C%EB%A6%AD%EC%9C%BC%EB%A1%9C-%EB%93%9C%EB%A1%A0-%EC%A0%9C%EC%96%B4%ED%95%98%EA%B8%B0/" target="_blank"> <img style="margin-right:20px;" src="img/20170802_droneimg_rokit_3.png"></a>
+                        <a href="http://rokitarduino.org/2016/03/04/%EC%95%84%EB%91%90%EC%9D%B4%EB%85%B8%EB%A1%9C-%EB%93%9C%EB%A1%A0-%EC%A0%9C%EC%96%B4%ED%95%98%EA%B8%B0/" target="_blank"> <img src="img/20170802_droneimg_rokit_4.png"></a></div>
+                        <img src="img/20170802_droneimg_rokit_1_2.png">
+                        <a href="http://robolink.co.kr/sw/droneplug/rokitdrone_droneplug.html"><img style="margin-top:60px;" src="http://robolink.co.kr/img/05_bt_usb2.png"></a>
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
