ef51984abbe2c4fb094aafa329f457879822e357
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:08:00 +0900

    news

---
+++ sub_3.php
@@ -0,0 +1,141 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <style>
+        .news_one {
+            height: 140px;
+            vertical-align: top;
+            margin: 20px 0 0 0;
+            padding-bottom: 20px;
+            border-bottom: solid 1px #e5e5e5;
+        }
+        
+        .news_img {
+            width: 140px;
+            height: 140px;
+            background-color: #666;
+            float: left;
+        }
+        
+        .detail {
+            font-family: "나눔 고딕", "Nanum Gothic", "맑은 고딕", "Malgun Gothic", "Apple Gothic", sans-serif;
+            padding: 0 0 0 30px;
+            margin: 0;
+            text-align: left;
+            float: left;
+            vertical-align: top;
+        }
+        
+        .detail_title {
+            font-family: "나눔 고딕", "Nanum Gothic", "맑은 고딕", "Malgun Gothic", "Apple Gothic", sans-serif;
+            font-size: 23px;
+            color: #000;
+            font-weight: bolder;
+            letter-spacing: -1.2px;
+            margin: 3px 0 15px 0;
+        }
+        
+        .detail_text {
+            font-family: "나눔 고딕", "Nanum Gothic", "맑은 고딕", "Malgun Gothic", "Apple Gothic", sans-serif;
+            font-size: 15px;
+            color: #666;
+            margin-bottom: 35px;
+            letter-spacing: -1.2px;
+            margin: 0;
+            line-height: 18px;
+        }
+        
+        .detail_date {
+            font-family: "나눔 고딕", "Nanum Gothic", "맑은 고딕", "Malguns Gothic", "Apple Gothic", sans-serif;
+            font-size: 14px;
+            margin: 30px 0 0 0;
+            color: #09a09e;
+        }
+    </style>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_1_03_news.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_1.php">회사소개</a> > <strong>뉴스</strong></p>
+            </div>
+            <div class="content">
+               <a href="20170801_news_robotcampchildren_photo.php">
+                    <div class="news_one">
+                        <tr>
+                            <td> <img class="news_img" src="http://robolink.co.kr/imgs/20170801_newsimg_17campphoto.png"> </td>
+                            <td>
+                                <div class="detail">
+                                    <p class="detail_title">[공지] 2017 어린이 로봇 캠프 사진</p>
+                                    <p class="detail_text">2017 어린이 로봇 캠프의 현장감이 한가득!
+                                        <br>캠프 사진을 다운받아보세요!</p>
+                                    <p class="detail_date">2017. 08. 01(화)</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                </a>
+                <a href="20170728_news_robotcampchildren.php">
+                    <div class="news_one">
+                        <tr>
+                            <td> <img class="news_img" src="http://robolink.co.kr/imgs/20170727_newsimg_17camp.png"> </td>
+                            <td>
+                                <div class="detail">
+                                    <p class="detail_title">[공지] 2017 어린이 로봇 캠프</p>
+                                    <p class="detail_text">2017 어린이 로봇 캠프를 소개합니다!
+                                        <br>신나게 드론 날리GO!! 캠프도 즐기GO!!</p>
+                                    <p class="detail_date">2017. 07. 26(수) ~ 07. 28(금)</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                </a>
+                <a href="20170727_news_cubeworksale.php">
+                    <div class="news_one">
+                        <tr>
+                            <td> <img class="news_img" src="http://robolink.co.kr/imgs/20170727_newsimg_cubeworks.png"> </td>
+                            <td>
+                                <div class="detail">
+                                    <p class="detail_title">[이벤트] 가정의 달 큐브웍스 신제품 할인 이벤트</p>
+                                    <p class="detail_text">가정의 달을 맞이하여 큐브웍스 0단계가 출시되었습니다.
+                                        <br>약 한달간의 이벤트 기간동안 특별할인가로 만나보세요!</p>
+                                    <p class="detail_date">2017. 05. 01(월) ~ 05. 31(수)</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                </a>
+                <a href="20170728_news_dronechampionship.php">
+                    <div class="news_one">
+                        <tr>
+                            <td> <img class="news_img" src="http://robolink.co.kr/imgs/20170728_newsimg_koreadronechapionship.png"> </td>
+                            <td>
+                                <div class="detail">
+                                    <p class="detail_title">[대회] 2016 코리아 드론 챔피언쉽</p>
+                                    <p class="detail_text">2016 코리아 드론 챔피언쉽 에서 자신만의 아이디어를 뽐내보세요!
+                                        <br>레이싱/ 경진대회/ 전시/ 체험 등 다양한 드론 컨텐츠</p>
+                                    <p class="detail_date">2016. 10. 22(토) ~ 10. 23(일)</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="news_one">
+                        <tr>
+                            <td> <img class="news_img" src="http://robolink.co.kr/imgs/20170727_newsimg_16camp.png"> </td>
+                            <td>
+                                <div class="detail">
+                                    <p class="detail_title">[공지] 2016 인천 어린이 로봇 캠프 행사 영상과 사진</p>
+                                    <p class="detail_text">2016 인천 어린이 로봇 캠프 행사의 현장 사진 및 영상을 모아모아~
+                                        <br>즐거웠던 로봇캠프의 현장을 공개합니다!</p>
+                                    <p class="detail_date">2016.08.20</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
