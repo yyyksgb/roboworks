596d97a5f8f730a61b16af0961cb20247a0f82e9
yyyksgb <30882899+yyyksgb@users.noreply.github.com>
2017-08-10 11:07:51 +0900

    history

---
+++ sub_2.php
@@ -0,0 +1,15 @@
+<?
+include_once dirname(__FILE__)."/top.php";
+?>
+    <center>
+        <div class="sub_main">
+            <div class="sub_title"> <img class="title_text" src="img/sub_title_1_02_history.png">
+                <p class="nav"><a href="http://www.roboworks.co.kr">홈</a> > <a href="http://www.roboworks.co.kr/sub_1.php">회사소개</a> > <strong>회사 연혁</strong></p>
+            </div>
+            <div class="content"> <img src="img/sub_img_1_history01.png">
+            <img src="img/sub_img_1_history02.png"></div>
+        </div>
+    </center>
+    <?
+include_once dirname(__FILE__)."/footer.php";
+?>
